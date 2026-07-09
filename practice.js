5a761956c1b063d434ee1559e09d75d3d9394b12
Muhammad Zaid <mzaidi1326@gmail.com>
2026-07-09 18:59:07 +0500

    Added form validation practice code

--- practice.js
+++ practice.js
@@ -1,15 +1,11 @@
-let ab = document.querySelector("#ab");
- ab.addEventListener("mouseover", function(){
-      ab.style.backgroundColor ="yellow";
-    });
-     ab.addEventListener("mouseout", function(){
-      ab.style.backgroundColor ="aqua";
-    });
-     window.addEventListener("mousemove", function(dets){
-      ab.style.top= dets.clientY + "px";
-       ab.style.left= dets.clientX + "px";
-    });
-let ul = document.querySelector("ul");
-ul.addEventListener("click" , function(dets){
-    dets.target.classList.toggle("lt");
+let name = document.querySelector("#name");
+let form = document.querySelector("form")
+form.addEventListener("submit" , function(dets){
+  dets.preventDefault();
+ if (name.value.length > 2) {
+  document.querySelector("#hide").style.display = "none";  
+  
+} else {
+    document.querySelector("#hide").style.display = "block";
+}
 });
